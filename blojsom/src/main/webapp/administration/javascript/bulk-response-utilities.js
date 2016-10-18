bc690a99e549a993e3c693f8a8cceb3ab369eb9f
Timothy Stone <javafueled@gmail.com>
2016-10-17 21:35:56 -0400

    Working response accordions in Bulk Management.

--- blojsom/src/main/webapp/administration/javascript/bulk-response-utilities.js
+++ blojsom/src/main/webapp/administration/javascript/bulk-response-utilities.js
@@ -1,20 +1,34 @@
 (function(Y) {
     var E = Y.util.Event,
         Dom = Y.util.Dom,
-        S = Y.util.Selector,
         Anim = Y.util.Anim;
-        
-        
+
         var pageInit = function() {
-            response_metas = S.query("blojsom-accordion-content-meta::before");
-            E.on(response_metas, 'click', function() {
-                console.log("click!");
+            var content_metas = Dom.getElementsByClassName("blojsom-accordion-content-meta"),
+                response_metas = Dom.getElementsByClassName("response_meta");
+
+            E.on(response_metas, 'click', function(e) {
+                E.stopPropagation(e);
             });
             
-            console.log(response_metas);
-        };
-        
+            E.on(content_metas, 'click', function(e) {
+                var target = e.target,
+                    sibling = Dom.getNextSibling(target),
+                    accordion_open = Dom.hasClass(target, 'blojsom-accordion-open'),
+                    open, close;
+                if(!accordion_open) {
+                    Dom.removeClass(target, 'blojsom-accordion-closed');
+                    Dom.addClass(target, 'blojsom-accordion-open');
+                    open = new Anim(sibling, {height:{to:sibling.scrollHeight}}, .1);
+                    open.animate();
+                } else { 
+                    Dom.removeClass(target, 'blojsom-accordion-open');
+                    Dom.addClass(target, 'blojsom-accordion-closed'); 
+                    close = new Anim(sibling, {height:{to:0}}, .1);
+                    close.animate();
+                }
+            });
+            
+        };  
         E.onDOMReady(pageInit);
-
-        
 })(YAHOO);
